1b6e18254d222b1f5e1d78334d78caf3a50d3a56
Anton Kashcheev <antonkachsheev@fmail.com>
2019-02-27 01:25:23 +0300

    Fix 'make help'.

--- make_rules/rules/rules_help.mk
+++ make_rules/rules/rules_help.mk
@@ -1,36 +1,38 @@
 ﻿help:
 	@echo Base names:
-	@echo "    NAME                        - Project name                        "
-	@echo "    BUILDPATH                   - Path to build directory             "
-	@echo "    BUILD_CONFIG                - 'execute', 'library' or 'multibuild'"
-	@echo "    BUILD_TYPE                  - 'release' or 'debug'                "
+	@echo "    NAME                        - Project name                     "
+	@echo "    BUILDPATH                   - Path to build directory          "
+	@echo "    BUILD_CONFIG                - 'execute', 'library' or          "
+	@echo "                               'multibuild'"
+	@echo "    BUILD_TYPE                  - 'release' or 'debug'             "
 	@echo "    SOURCE_LIST                 - "
-	@echo "    SOURCE_PATH                 - "
+	@echo "    SOURCE_PATH_LIST            - ?"
 	@echo "    INSTALL_PATH                - ?"
 	@echo
 	@echo Logging:
-	@echo "    LOG_VARS                    - ? 'true' or 'false'                "
-	@echo "    LOG_RULES                   - ? 'true' or 'false'                "
-	@echo "    LOG_COMMANDS                - ? 'true' or 'false'                "
-	@echo "    LOG_ALL                     - ? 'true' or 'false'                "
+	@echo "    LOG_VARS                    - ? 'true' or 'false'              "
+	@echo "    LOG_RULES                   - ? 'true' or 'false'              "
+	@echo "    LOG_COMMANDS                - ? 'true' or 'false'              "
+	@echo "    LOG_ALL                     - ? 'true' or 'false'              "
 	@echo
 	@echo Base language properties:
-	@echo "    LANG                        - 'c', 'cpp'"
+	@echo "    LANG                        - 'c', 'cpp'                       "
 	@echo
 	@echo C language properties:
-	@echo "    CMP_TYPE                    - 'gcc', 'clang' or 'cl.exe'          "
-	@echo "    INCLUDE_PATHS               - "
-	@echo "    CC_FLAGS                    - "
+	@echo "    CMP_TYPE                    - 'gcc', 'clang' or 'cl.exe'       "
+	@echo "    INCLUDE_PATH_LIST           - ?"
+	@echo "    CC_FLAG_LIST                - ?"
 	@echo "    C_STANDART                  - ?"
 	@echo "    INSTALL_HEADER_PATH         - ?"
 	@echo "    INSTALL_HEADER_LIST         - ?"
 	@echo
 	@echo C++ language properties:
-	@echo "    CMP_TYPE                    - 'gcc', 'clang' or 'cl.exe'          "
-	@echo "    INCLUDE_PATHS               - "
-	@echo "    CXX_FLAGS                   - "
+	@echo "    CMP_TYPE                    - 'gcc', 'clang' or 'cl.exe'       "
+	@echo "    INCLUDE_PATH_LIST           - ?"
+	@echo "    CXX_FLAG_LIST               - ?"
 	@echo "    CXX_STANDART                - ?"
 	@echo "    INSTALL_HEADER_PATH         - ?"
 	@echo "    INSTALL_HEADER_LIST         - ?"
-
-
+	@echo
+	@echo Multibuild options:
+	@echo "    PROJECT_LIST                - ?"
